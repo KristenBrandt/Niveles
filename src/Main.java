bdfc5b471805f7f125c6f51548bc2da0dd28eb8b
KristenBrandt <kabrandtf@gmail.com>
2018-07-16 23:14:38 -0600

    Programa

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     private static Scanner scan = new Scanner(System.in);
 
+    /** menu principal **/
     public static String opciones() {
         String a = "Menu de opciones principales \n\t 1:Agregar Nivel \n\t 2:Agregar Grado \n\t 3:Agregar Estudiante \n\t 4:Ver grados en un un nivel \n\t 5:Ver asignaciones en un grado \n\t 6:Salir";
         return a;
     }
 
     public static void main(String[] args){
+
+        /** variables **/
         boolean pony = true;
         String nivi;
         String gradu;
@@ -17,18 +20,23 @@ public class Main {
         String codi;
         Colegio cole = new Colegio();
 
+        /** loop general **/
         do{
             System.out.println(opciones());
     String opcion = scan.nextLine();
     switch (opcion){
+
+        /** agregar nivel **/
         case "1":
             System.out.println("Ingrese el nombre del nivel");
             nivi = scan.nextLine();
             Nivel nivel = new Nivel(nivi);
             cole.agregarnivel(nivel);
             System.out.println("Ha agregado un nivel exitosamente \n");
+
             break;
 
+            /** agregar grado **/
         case "2":
             System.out.println("Ingrese el nombre del nivel");
             nivi = scan.nextLine();
@@ -44,6 +52,7 @@ public class Main {
             }
             break;
 
+            /** agregar estudiante **/
         case "3":
             System.out.println("Ingrese el nombre del nivel");
             nivi = scan.nextLine();
@@ -66,6 +75,7 @@ public class Main {
             }
             break;
 
+            /** ver grados en un nivel **/
         case "4":
             System.out.println("Ingrese el nombre del nivel");
             nivi = scan.nextLine();
@@ -80,6 +90,7 @@ public class Main {
             }
             break;
 
+            /** ver asignaciones en un grado **/
         case "5":
             System.out.println("Ingrese el nombre del nivel");
             nivi = scan.nextLine();
@@ -99,18 +110,21 @@ public class Main {
             }
             break;
 
+            /** salir del programa **/
         case "6":
             System.out.println("Cerrando Programa");
             pony = false;
             break;
 
+            /** por si usuario no ingresa alguno de los numeros deseados **/
             default:
+                System.out.println("Debe de ingresar un numero del 1-6, intente denuevo \n");
                 break;
 
 
     }
-    
 
+/** para cerrar programa **/
     }while (pony);
     }
 }
